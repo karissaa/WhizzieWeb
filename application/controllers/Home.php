e9c1aa022f432eda97cd96df5e5dd8e1b4404e04
Felicia Karissa <feliciakarissa@gmail.com>
2019-04-20 15:42:17 +0700

    Sudah bisa buka Home

---
+++ application/controllers/Home.php
@@ -0,0 +1,21 @@
+<?php
+
+    defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class Home extends CI_Controller{
+
+        function __Construct(){
+            parent::__Construct ();
+                $this->load->database();
+                $this->load->model('home_model');
+        }
+
+        public function index(){
+            $data['js'] = $this->load->view('include/javascript.php', NULL, TRUE);
+            $data['css'] = $this->load->view('include/css.php', NULL, TRUE);
+            //$data['product'] = $this->home_model->get_product();
+
+            $this->load->view('pages/homeview.php', $data);
+        }
+    }
+?>
---
+++ application/views/include/css.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/bootstrap.min.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/font-awesome.min.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/nouislider.min.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/slick.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/slick-theme.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('/assets/css/style.css'); ?>">
