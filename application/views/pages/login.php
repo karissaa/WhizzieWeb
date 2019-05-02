ee196ccf662b56c94c21c058c650e103161d9ae2
Snnopys <43606736+Snnopys@users.noreply.github.com>
2019-05-02 22:56:02 +0700

    Create Login Page
    Login Page (alpha)

---
+++ application/views/pages/login.php
@@ -0,0 +1,27 @@
+<?php
+    echo form_open_multipart(''); // isinya untuk nembak post
+    
+    echo form_label('Username', 'username');
+    $Username = array(
+        'type' => 'text',
+        'name' => 'loginUsername',
+        'placeholder' => 'Input Your Username...',
+        'class' => 'form-control'
+    );
+    echo form_input($Username);
+
+    echo form_label('Password', 'passwd');
+    $password = array(
+        'name' => 'loginPassword',
+        'class' => 'form-control'
+    );
+    echo form_password($password);
+
+    $btnSubmit = array(
+        'class' => 'btn btn-primary'
+    );
+    echo form_submit($btnSubmit);
+
+    echo form_close();
+
+?>
