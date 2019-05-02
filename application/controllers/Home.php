505427e363644bcf7fe36d881e7105d553016abc
Snnopys <43606736+Snnopys@users.noreply.github.com>
2019-05-02 23:28:07 +0700

    Update Controller untuk Add User
    Add Data function belum ada, form validation masih belum berjalan dengan baik, masih berantakkan

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -13,5 +13,31 @@
             $data['css'] = $this->load->view('include/css.php', NULL, TRUE);
             $this->load->view('pages/homeview.php', $data);
         }
+
+        public function AddUser(){
+            $data['js'] = $this->load->view('include/javascript.php', NULL, TRUE);
+            $data['css'] = $this->load->view('include/css.php', NULL, TRUE);
+
+            $username = $this->input->post('loginUsername');
+            $password = $this->input->post('loginPassword');
+            $passwordConf = $this->input->post('loginPasswordConfirm');
+
+            $this->form_validation->set_rules('loginUsername'	 ,     'Login Username'  ,             'required',			     array('required'=> 'This field cannot be Empty'));
+            $this->form_validation->set_rules('loginPassword'	 ,     'Login Password',               'required|min_length[8]', array('required'=> 'This field cannot be Empty', 'min_length[8]' => 'Please input minimal 8 character!'));
+            $this->form_validation->set_rules('loginPasswordConfirm',  'Login Password Confirmation',  'required',				 array('required'=> 'This field cannot be Empty'));
+
+            if($this->form_validation->run() == FALSE){
+                $this->load->view('pages/signup.php', $data);
+            } else{
+
+                if($password != $passwordConf){
+                    //Password tidak match, Print Error!
+                    redirect('pages/signup.php');
+                }else{
+                    // AddData($username, $password, $passwordConf) dari data yang sudah diinput
+                }
+
+            }
+        }
     }
 ?>
