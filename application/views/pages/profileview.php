19da15e2855b041acf8c5e0eefe984f10de3534d
Felicia Karissa <feliciakarissa@gmail.com>
2019-05-12 11:12:13 +0700

    Profile dah oke

--- application/views/pages/profileview.php
+++ application/views/pages/profileview.php
@@ -23,7 +23,7 @@
 		<?=$header?>
 		<!-- /HEADER -->
 
-		<div id="hot-deal" class="section"style="height:100px; margin-top: 0; background:#51b4b6; background-size: cover;">
+		<div id="hot-deal" class="section"style="height:100px; margin: 0; background:#51b4b6; background-size: cover;">
 		</div>
 
 		<!-- NAVIGATION -->
@@ -109,76 +109,109 @@
 				
 				<div class="row">
 					<!-- product -->
-					<div class="col-md-4 col-xs-6">
+					<div class="col-md-3 col-xs-6">
 						<div class="product">
 							<div class="product-img">
 								<img src="../../assets/img/001.jpg" alt="">
 							</div>
 							<div class="product-body">
-								<p class="product-category">Category</p>
-								<h3 class="product-name"><a href="#">product name goes here</a></h3>
-								<h4 class="product-price">$980.00 <del class="product-old-price">$990.00</del></h4>
-								<div class="product-rating">
-								</div>
-								<div class="product-btns">
-									<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
-									<button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span></button>
-									<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
+								<p class="product-category">Men Fashion</p>
+								<h3 class="product-name"><a href="#">Wooden Mahogany Watch</a></h3>
+								<p>Really need one for my wedding!</p>
+								<div class="row">
+									<button class="btn"><b>8 Offers</b></button>
+									<div class="btn-group product-btns">
+										<button type="button" class="add-to-wishlist dropdown-toggle" data-toggle="dropdown">
+											<i class="fa fa-ellipsis-v"></i><span class="tooltipp">Settings</span>
+										</button>
+										<div class="dropdown-menu">
+											<a class="dropdown-item" href="#">Edit</a>
+											<a class="dropdown-item" href="#">Delete</a>
+										</div>
+									</div>
 								</div>
 							</div>
-							<!-- <div class="add-to-cart">
-								<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
-							</div> -->
 						</div>
 					</div>
 					<!-- /product -->
 
 					<!-- product -->
-					<div class="col-md-4 col-xs-6">
+					<div class="col-md-3 col-xs-6">
 						<div class="product">
 							<div class="product-img">
 								<img src="../../assets/img/gigi.jpg" alt="">
 							</div>
 							<div class="product-body">
-								<p class="product-category">Category</p>
-								<h3 class="product-name"><a href="#">product name goes here</a></h3>
-								<h4 class="product-price">$980.00 <del class="product-old-price">$990.00</del></h4>
-								<div class="product-rating">
-								</div>
-								<div class="product-btns">
-									<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
-									<button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span></button>
-									<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
+								<p class="product-category">Men Fashion</p>
+								<h3 class="product-name"><a href="#">Wooden Mahogany Watch</a></h3>
+								<p>Really need one for my wedding!</p>
+								<div class="row">
+									<button class="btn"><b>8 Offers</b></button>
+									<div class="btn-group product-btns">
+										<button type="button" class="add-to-wishlist dropdown-toggle" data-toggle="dropdown">
+											<i class="fa fa-ellipsis-v"></i><span class="tooltipp">Settings</span>
+										</button>
+										<div class="dropdown-menu">
+											<a class="dropdown-item" href="#">Edit</a>
+											<a class="dropdown-item" href="#">Delete</a>
+										</div>
+									</div>
 								</div>
 							</div>
-							<!-- <div class="add-to-cart">
-								<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
-							</div> -->
 						</div>
 					</div>
 					<!-- /product -->
 
 					<!-- product -->
-					<div class="col-md-4 col-xs-6">
+					<div class="col-md-3 col-xs-6">
 						<div class="product">
 							<div class="product-img">
 								<img src="../../assets/img/009.jpg" alt="">
 							</div>
 							<div class="product-body">
-								<p class="product-category">Category</p>
-								<h3 class="product-name"><a href="#">product name goes here</a></h3>
-								<h4 class="product-price">$980.00 <del class="product-old-price">$990.00</del></h4>
-								<div class="product-rating">
-								</div>
-								<div class="product-btns">
-									<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
-									<button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span></button>
-									<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
+								<p class="product-category">Men Fashion</p>
+								<h3 class="product-name"><a href="#">Wooden Mahogany Watch</a></h3>
+								<p>Really need one for my wedding!</p>
+								<div class="row">
+									<button class="btn"><b>8 Offers</b></button>
+									<div class="btn-group product-btns">
+										<button type="button" class="add-to-wishlist dropdown-toggle" data-toggle="dropdown">
+											<i class="fa fa-ellipsis-v"></i><span class="tooltipp">Settings</span>
+										</button>
+										<div class="dropdown-menu">
+											<a class="dropdown-item" href="#">Edit</a>
+											<a class="dropdown-item" href="#">Delete</a>
+										</div>
+									</div>
 								</div>
 							</div>
-							<!-- <div class="add-to-cart">
-								<button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
-							</div> -->
+						</div>
+					</div>
+					<!-- /product -->
+
+					<!-- product -->
+					<div class="col-md-3 col-xs-6">
+						<div class="product">
+							<div class="product-img">
+								<img src="../../assets/img/010.jpg" alt="" style="object-fit: cover; width: 260px; height: 260px;">
+							</div>
+							<div class="product-body">
+								<p class="product-category">Men Fashion</p>
+								<h3 class="product-name"><a href="#">Wooden Mahogany Watch</a></h3>
+								<p>Really need one for my wedding!</p>
+								<div class="row">
+									<button class="btn"><b>8 Offers</b></button>
+									<div class="btn-group product-btns">
+										<button type="button" class="add-to-wishlist dropdown-toggle" data-toggle="dropdown">
+											<i class="fa fa-ellipsis-v"></i><span class="tooltipp">Settings</span>
+										</button>
+										<div class="dropdown-menu">
+											<a class="dropdown-item" href="#">Edit</a>
+											<a class="dropdown-item" href="#">Delete</a>
+										</div>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 					<!-- /product -->
@@ -191,140 +224,6 @@
 			<!-- /container -->
 		</div>
 		<!-- /SECTION -->
-
-		<!-- NEWSLETTER -->
-		<div id="newsletter" class="section">
-			<!-- container -->
-			<div class="container">
-				<!-- row -->
-				<div class="row">
-					<div class="col-md-12">
-						<div class="newsletter">
-							<p>Sign Up for the <strong>NEWSLETTER</strong></p>
-							<form>
-								<input class="input" type="email" placeholder="Enter Your Email">
-								<button class="newsletter-btn"><i class="fa fa-envelope"></i> Subscribe</button>
-							</form>
-							<ul class="newsletter-follow">
-								<li>
-									<a href="#"><i class="fa fa-facebook"></i></a>
-								</li>
-								<li>
-									<a href="#"><i class="fa fa-twitter"></i></a>
-								</li>
-								<li>
-									<a href="#"><i class="fa fa-instagram"></i></a>
-								</li>
-								<li>
-									<a href="#"><i class="fa fa-pinterest"></i></a>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<!-- /row -->
-			</div>
-			<!-- /container -->
-		</div>
-		<!-- /NEWSLETTER -->
-
-		<!-- FOOTER -->
-		<footer id="footer">
-			<!-- top footer -->
-			<div class="section">
-				<!-- container -->
-				<div class="container">
-					<!-- row -->
-					<div class="row">
-						<div class="col-md-3 col-xs-6">
-							<div class="footer">
-								<h3 class="footer-title">About Us</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>
-								<ul class="footer-links">
-									<li><a href="#"><i class="fa fa-map-marker"></i>1734 Stonecoal Road</a></li>
-									<li><a href="#"><i class="fa fa-phone"></i>+021-95-51-84</a></li>
-									<li><a href="#"><i class="fa fa-envelope-o"></i>email@email.com</a></li>
-								</ul>
-							</div>
-						</div>
-
-						<div class="col-md-3 col-xs-6">
-							<div class="footer">
-								<h3 class="footer-title">Categories</h3>
-								<ul class="footer-links">
-									<li><a href="#">Hot deals</a></li>
-									<li><a href="#">Laptops</a></li>
-									<li><a href="#">Smartphones</a></li>
-									<li><a href="#">Cameras</a></li>
-									<li><a href="#">Accessories</a></li>
-								</ul>
-							</div>
-						</div>
-
-						<div class="clearfix visible-xs"></div>
-
-						<div class="col-md-3 col-xs-6">
-							<div class="footer">
-								<h3 class="footer-title">Information</h3>
-								<ul class="footer-links">
-									<li><a href="#">About Us</a></li>
-									<li><a href="#">Contact Us</a></li>
-									<li><a href="#">Privacy Policy</a></li>
-									<li><a href="#">Orders and Returns</a></li>
-									<li><a href="#">Terms & Conditions</a></li>
-								</ul>
-							</div>
-						</div>
-
-						<div class="col-md-3 col-xs-6">
-							<div class="footer">
-								<h3 class="footer-title">Service</h3>
-								<ul class="footer-links">
-									<li><a href="#">My Account</a></li>
-									<li><a href="#">View Cart</a></li>
-									<li><a href="#">Wishlist</a></li>
-									<li><a href="#">Track My Order</a></li>
-									<li><a href="#">Help</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<!-- /row -->
-				</div>
-				<!-- /container -->
-			</div>
-			<!-- /top footer -->
-
-			<!-- bottom footer -->
-			<div id="bottom-footer" class="section">
-				<div class="container">
-					<!-- row -->
-					<div class="row">
-						<div class="col-md-12 text-center">
-							<ul class="footer-payments">
-								<li><a href="#"><i class="fa fa-cc-visa"></i></a></li>
-								<li><a href="#"><i class="fa fa-credit-card"></i></a></li>
-								<li><a href="#"><i class="fa fa-cc-paypal"></i></a></li>
-								<li><a href="#"><i class="fa fa-cc-mastercard"></i></a></li>
-								<li><a href="#"><i class="fa fa-cc-discover"></i></a></li>
-								<li><a href="#"><i class="fa fa-cc-amex"></i></a></li>
-							</ul>
-							<span class="copyright">
-								<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-								Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-							</span>
-
-
-						</div>
-					</div>
-						<!-- /row -->
-				</div>
-				<!-- /container -->
-			</div>
-			<!-- /bottom footer -->
-		</footer>
-		<!-- /FOOTER -->
 
 		<?=$js?>
 		
